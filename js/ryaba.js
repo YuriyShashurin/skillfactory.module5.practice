105b639d4fe08c69a6b933e1e275dab1a0340c2f
YuriyShashurin <47944277+YuriyShashurin@users.noreply.github.com>
2020-05-03 20:01:03 +0300

    Add files via upload

---
+++ js/ryaba.js
@@ -0,0 +1,40 @@
+const dataURL = "https://api.jsonbin.io/b/5e905926172eb643896166e7";
+
+
+function handleButton() {
+	$.getJSON(dataURL, handleData);
+	
+
+}
+
+function handleData(data) {
+	let message = "";
+	let obj = { //забираем в объект значения формы/ 
+		var1: $("input[name=var1]")[0].value,
+		var2: $("input[name=var2]")[0].value,
+		var3: $("input[name=var3]")[0].value,
+		var4: $("input[name=var4]")[0].value,
+		var5: $("input[name=var5]")[0].value,
+		var6: $("input[name=var6]")[0].value,
+		speach: $("input[name=speach]")[0].value
+
+
+	}
+	data["text"].forEach(function(item, index) {
+		for (key in obj) {
+			item = item.replace("{" + key +"}", obj[key]); //проходим по объекту text и меняем значения на взятые из свойств obj
+		}
+	
+		
+	message = message + item + "<BR>";
+
+	});
+
+	$("#result").html(message); 
+}
+
+function init() {
+	$("#button-fetch").click(handleButton);
+}
+
+$(document).ready(init);
